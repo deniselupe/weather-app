d3653aeb5aec783ac3b619230462217bab2b857f
deniselupe <dgrodriguez24@hotmail.com>
2024-01-11 02:34:51 +0000

    :lipstick: style header search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex flex-col bg-slate-900">
       <Header />
-      <div className="w-5/6 md:w-3/4 mx-auto border border-black">
+      <div className="w-5/6 md:w-3/4 mx-auto flex-1 border">
         <div id="current"></div>
         <div id="forecast"></div>
       </div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,19 @@
 export default function Header() {
     return (
-        <header className="text-black my-6 border border-black">
-            <div className="w-5/6 md:w-3/4 mx-auto flex justify-between items-center">
-                <div className="w-40 min-w-40 text-2xl">Weather</div>
-                <div className="flex-1 ml-5">
-                    <div className="flex gap-2 justify-end items-center">
-                        <input type="text" placeholder="City, State, Zip Code" />
-                        <button>Search</button>
-                    </div>
+        <header className="text-white font-light my-6">
+            <div className="w-5/6 md:w-3/4 mx-auto text-black relative">
+                <div id="search-bar" className="md:w-[400px] ml-auto flex gap-2 justify-end items-center bg-white rounded-t">
+                    <input
+                        type="text"
+                        placeholder="City, State, Zip Code"
+                        className="flex-1 px-4 py-2 border outline-none rounded"
+                    />
+                </div>
+                <div id="results-dropdown" className="w-full md:w-[400px] fixed absolute right-0 bg-white rounded-b">
+                    <div id="dropdown-item" className="px-4 py-2 hover:bg-zinc-100 cursor-pointer">City, State, Country</div>
+                    <div id="dropdown-item" className="px-4 py-2 hover:bg-zinc-100 cursor-pointer">City, State, Country</div>
+                    <div id="dropdown-item" className="px-4 py-2 hover:bg-zinc-100 cursor-pointer">City, State, Country</div>
+                    <div id="dropdown-item" className="px-4 py-2 hover:bg-zinc-100 cursor-pointer">City, State, Country</div>
                 </div>
             </div>
         </header>
