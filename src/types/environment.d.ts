280c3fbdeca82388dba418b66fc08b72dbc6e42b
deniselupe <dgrodriguez24@hotmail.com>
2024-01-31 05:03:10 +0000

    :label: change path for geolocation.ts

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,6 +3,7 @@ declare global {
         interface ProcessEnv {
             WEATHER_API_KEY: string;
             GEOCODE_URL: string;
+            CURRENT_WEATHER_URL: string;
         }
     }
 }
