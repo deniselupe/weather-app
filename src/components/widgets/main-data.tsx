535a6fd3a2d186129390eec2a741dd3976c01298
deniselupe <dgrodriguez24@hotmail.com>
2024-02-01 22:37:29 +0000

    :construction: fetch and render main widget data

--- src/components/widgets/main-data.tsx
+++ src/components/widgets/main-data.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+import { useWeatherContext } from "@/contexts/weather";
+
 export default function MainWeatherWidget() {
-    return (
-        <div className="md:w-96 md:h-96 mb-2 bg-gray-800 text-white flex flex-col items-center justify-center">
-            <h2 className="text-xl">My Location</h2>
-            <p className="text-xs">City, State</p>
-            <h1 className="text-6xl">63°</h1>
-            <p>Cloudy</p>
-            <p>High: 63° Low: 38°</p>
-        </div>
-    );
+    const { fetchMainWidgetData } = useWeatherContext();
+    const data = fetchMainWidgetData();
+
+    if (data === null) {
+        return null;
+    } else {
+        return (
+            <div className="md:w-96 md:h-96 mb-2 flex flex-col items-center justify-center text-white border">
+                <h2 className="text-2xl">My Location</h2>
+                <p className="text-md">{data.location}</p>
+                <h1 className="text-8xl">{data.currTemp}°</h1>
+                <p className="text-md">{data.description}</p>
+                <p className="text-md">High: {data.maxTemp}° Low: {data.minTemp}°</p>
+            </div>
+        );
+    }
 }
