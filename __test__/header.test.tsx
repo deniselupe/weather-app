5f7c66ec51f384b429db3f71223d2baafb72a16b
deniselupe <dgrodriguez24@hotmail.com>
2024-02-01 20:11:21 +0000

    :white_check_mark: check if input updates value on change

--- __test__/header.test.tsx
+++ __test__/header.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Header from "@/components/header";
 
 describe("Header", () => {
@@ -7,5 +7,19 @@ describe("Header", () => {
 
         const input = screen.getByPlaceholderText("Search city...");
         expect(input).toBeInTheDocument();
+    });
+
+    test("handles input change", () => {
+        render(<Header />);
+
+        const input = screen.getByPlaceholderText("Search city...") as HTMLInputElement;
+
+        fireEvent.change(input, {
+            target: {
+                value: "New York"
+            }
+        });
+
+        expect(input.value).toBe("New York");
     });
 });
