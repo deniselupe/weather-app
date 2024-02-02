2a6ab47820f49694ed8439ca17480e67590a7059
deniselupe <dgrodriguez24@hotmail.com>
2024-02-02 07:20:02 +0000

    :bug: update response type in weather/api route handler

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server";
-import { CurrentWeatherObjType } from "@/types/weather";
+import { WeatherDataObjType } from "@/types/weather";
 
 export async function GET(req: NextRequest) {
     const url = process.env.CURRENT_WEATHER_URL;
@@ -8,12 +8,12 @@ export async function GET(req: NextRequest) {
     const latitude = searchParams.get("lat") as string;
     const longitude = searchParams.get("lon") as string;
 
-    const currentWeatherResponse = await fetch(`${url}?lat=${latitude}&lon=${longitude}&units=imperial&appid=${key}`);
+    const currentWeatherResponse = await fetch(`${url}?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&units=imperial&appid=${key}`);
 
     if (currentWeatherResponse.status !== 200) {
         return Response.json({ error: "Something went wrong with the request." }, { status: 400 });
     } else {
-        const currentWeatherData: CurrentWeatherObjType = await currentWeatherResponse.json();
+        const currentWeatherData: WeatherDataObjType = await currentWeatherResponse.json();
         return Response.json(currentWeatherData);
     }
 }
