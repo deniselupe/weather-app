c2f9318f6c03e4db8f23d4275bfd838563335069
deniselupe <dgrodriguez24@hotmail.com>
2024-02-02 02:44:37 +0000

    :construction: fetch and render current widget data

--- src/components/widgets/current-data.tsx
+++ src/components/widgets/current-data.tsx
@@ -1,33 +1,42 @@
 "use client";
 
+import { useWeatherContext } from "@/contexts/weather";
+
 export default function CurrentWeatherWidget() {
-    return (
-        <div className="md:min-w-[420px] mb-2 flex flex-wrap justify-around md:grid md:grid-rows-2 md:grid-cols-3 gap-4 text-white">
-            <div id="feels-like" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
-                <h2 className="text-xs">FEELS LIKE</h2>
-                <p className="text-3xl">40°</p>
+    const { fetchCurrentData } = useWeatherContext();
+    const data = fetchCurrentData();
+
+    if (data === null) {
+        return null;
+    } else {
+        return (
+            <div className="md:min-w-[420px] mb-2 flex flex-wrap justify-around md:grid md:grid-rows-2 md:grid-cols-3 gap-4 text-white">
+                <div id="feels-like" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
+                    <h2 className="text-xs">FEELS LIKE</h2>
+                    <p className="text-3xl">{data.feelsLike}°</p>
+                </div>
+                <div id="humidity" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
+                    <h2 className="text-xs">HUMIDITY</h2>
+                    <p className="text-3xl">{data.humidity}%</p>
+                </div>
+                <div id="pressure" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
+                    <h2 className="text-xs">PRESSURE</h2>
+                    <p className="text-3xl">{data.pressure} inHg</p>
+                </div>
+                <div id="sunrise" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
+                    <h2 className="text-xs">SUNRISE</h2>
+                    <p className="text-2xl">{data.sunrise}</p>
+                    <p className="mt-6 text-xs">Sunset: {data.sunset}</p>
+                </div>
+                <div id="wind-speed" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
+                    <h2 className="text-xs">WIND SPEED</h2>
+                    <p className="text-3xl">{data.windSpeed} MPH</p>
+                </div>
+                <div id="uv-index" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
+                    <h2 className="text-xs">UV INDEX</h2>
+                    <p className="text-3xl">{data.uvIndex}</p>
+                </div>
             </div>
-            <div id="humidity" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
-                <h2 className="text-xs">HUMIDITY</h2>
-                <p className="text-3xl">40°</p>
-            </div>
-            <div id="pressure" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
-                <h2 className="text-xs">PRESSURE</h2>
-                <p className="text-3xl">40°</p>
-            </div>
-            <div id="sunrise" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
-                <h2 className="text-xs">SUNRISE</h2>
-                <p className="text-2xl">7:06 AM</p>
-                <p className="mt-6 text-xs">Sunset: 5:42 PM</p>
-            </div>
-            <div id="wind-speed" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
-                <h2 className="text-xs">WIND SPEED</h2>
-                <p className="text-3xl">40°</p>
-            </div>
-            <div id="uv-index" className="w-32 h-32 p-4 bg-gray-800 rounded-3xl">
-                <h2 className="text-xs">UV INDEX</h2>
-                <p className="text-3xl">40°</p>
-            </div>
-        </div>
-    );
+        );
+    }
 }
