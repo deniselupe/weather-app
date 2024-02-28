e2d9dee13319c1a19c0aae44efd8e6a321006001
deniselupe <dgrodriguez24@hotmail.com>
2024-02-28 04:08:57 +0000

    :white_check_mark: test if header calls fetch on input value change

--- __test__/header.test.tsx
+++ __test__/header.test.tsx
@@ -1,25 +1,52 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Header from "@/components/header";
 
 describe("Header", () => {
-    test("renders input field", () => {
-        render(<Header />);
-
-        const input = screen.getByPlaceholderText("Search city...");
-        expect(input).toBeInTheDocument();
+    beforeAll(() => {
+        global.fetch = jest.fn(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve([
+                    {
+                        "name": "Houston",
+                        "lat": 37.3261588,
+                        "lon": -91.955988,
+                        "country": "US",
+                        "state": "Missouri"
+                    },
+                    {
+                        "name": "Houston",
+                        "lat": 61.6302778,
+                        "lon": -149.818055,
+                        "country": "US",
+                        "state": "Alaska"
+                    }
+                ])
+            });
+        }) as jest.Mock;
     });
 
-    test("handles input change", () => {
+    afterEach(() => jest.clearAllMocks());
+
+    test("renders input field", () => {
         render(<Header />);
-
-        const input = screen.getByPlaceholderText("Search city...") as HTMLInputElement;
-
-        fireEvent.change(input, {
-            target: {
-                value: "New York"
-            }
+    
+        const input = screen.getByPlaceholderText("Search city...");
+        
+        expect(input).toBeInTheDocument();
+    });
+    
+    test("calls fetch when input value is changed", async () => {
+        const user = userEvent.setup();
+    
+        render(<Header />);
+    
+        const input = screen.getByPlaceholderText("Search city...");
+    
+        await user.type(input, "New York");
+    
+        await waitFor(() => {
+            expect(global.fetch).toHaveBeenCalledTimes(1);
         });
-
-        expect(input.value).toBe("New York");
     });
 });
