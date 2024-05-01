2fca3769be2e2430e6c9b1d50e86e80d300aac2d
deniselupe <dgrodriguez24@hotmail.com>
2024-05-01 18:33:58 +0000

    :recycle: update styling for visibility widget

--- src/components/widgets/visibility.tsx
+++ src/components/widgets/visibility.tsx
@@ -1,12 +1,28 @@
+import VisibilityIcon from "@/public/svgs/visibility.svg";
+
 interface VisibilityProps {
     visibility: number;
 }
 
 export default function Visibility({ visibility }: VisibilityProps) {
+    const visibilityDescription = () => {
+        if (visibility >= 10) {
+            return "It's perfectly clear right now.";
+        } else if (visibility >= 5) {
+            return "Good visibility.";
+        } else {
+            return "Poor visibility. Exercise caution while driving or moving around.";
+        }
+    };
+
     return (
-        <div id="visibility" className="h-44 p-8 col-span-1 grow bg-gray-800 rounded-3xl">
-            <h2 className="text-xs">VISIBILITY</h2>
+        <div id="visibility" className="h-44 p-8 col-span-1 grow flex flex-col justify-between bg-gray-800 rounded-3xl">
+            <div className="flex items-center gap-2">
+                <VisibilityIcon className="w-4 h-4" />
+                <h2 className="text-xs">VISIBILITY</h2>
+            </div>
             <p className="text-2xl">{visibility / 1000} km</p>
+            <p className="text-xs">{visibilityDescription()}</p>
         </div>
     );
 }
