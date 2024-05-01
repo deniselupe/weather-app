98d9eff946b9a657d803ab7449e553238ba6bf23
deniselupe <dgrodriguez24@hotmail.com>
2024-05-01 17:05:59 +0000

    :recycle: add description for humidity widget

--- src/components/widgets/humidity.tsx
+++ src/components/widgets/humidity.tsx
@@ -1,12 +1,27 @@
+import HumidityIcon from "@/public/svgs/humidity.svg";
+
 interface HumidityProps {
     humidity: number;
 }
 
 export default function Humidity({ humidity }: HumidityProps) {
+    const humidityDescription = () => {
+        if (humidity < 40) {
+            return "Low humidity. It might feel dry.";
+        } else if (humidity < 70) {
+            return "Moderate humidity. Comfortable conditions.";
+        } else {
+            return "High humidity. It might feel humid and uncomfortable.";
+        }
+    };
     return (
-        <div id="humidity" className="h-44 p-8 col-span-1 grow bg-gray-800 rounded-3xl">
-            <h2 className="text-xs">HUMIDITY</h2>
+        <div id="humidity" className="h-44 p-8 col-span-1 grow flex flex-col justify-between bg-gray-800 rounded-3xl">
+            <div className="flex items-center gap-2">
+                <HumidityIcon className="w-4 h-4" />
+                <h2 className="text-xs">HUMIDITY</h2>
+            </div>
             <p className="text-2xl">{humidity}%</p>
+            <p className="text-xs">{humidityDescription()}</p>
         </div>
     );
 }
