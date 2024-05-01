6a4bb46c7fbf0cc44812cdd7cfd0894ac5c15768
deniselupe <dgrodriguez24@hotmail.com>
2024-05-01 17:41:14 +0000

    :recycle: add description for pressure widget

--- src/components/widgets/pressure.tsx
+++ src/components/widgets/pressure.tsx
@@ -1,12 +1,28 @@
+import PressureIcon from "@/public/svgs/pressure.svg";
+
 interface PressureProps {
-    pressure: string;
+    pressure: number;
 }
 
 export default function Pressure({ pressure }: PressureProps) {
+    const pressureDescription = () => {
+        if (pressure < 1000) {
+            return "Low pressure. Expect changes in the weather.";
+        } else if (pressure >= 1000 && pressure <= 1010) {
+            return "Normal pressure. Typical weather conditions.";
+        } else {
+            return "High pressure. Expect stable and clear weather.";
+        }
+    };
+    
     return (
-        <div id="pressure" className="h-44 p-8 col-span-1 grow bg-gray-800 rounded-3xl">
-            <h2 className="text-xs">PRESSURE</h2>
-            <p className="text-2xl">{pressure} inHg</p>
+        <div id="pressure" className="h-44 p-8 col-span-1 grow flex flex-col justify-between bg-gray-800 rounded-3xl">
+            <div className="flex items-center gap-2">
+                <PressureIcon className="w-4 h-4" />
+                <h2 className="text-xs">PRESSURE</h2>
+            </div>
+            <p className="text-2xl">{pressure} hPa</p>
+            <p className="text-xs">{pressureDescription()}</p>
         </div>
     );
 }
