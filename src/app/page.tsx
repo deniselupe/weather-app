011e961a67adeffc92aead4363a2810e9c2259e8
deniselupe <dgrodriguez24@hotmail.com>
2024-06-17 18:49:09 +0000

    :sparkles: introduce other large cities widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
 import Header from "@/components/header";
 import Footer  from "@/components/footer";
 import MainWeatherWidget from "@/components/widgets/main-data";
-import CurrentWeatherWidget from "@/components/widgets/current-data";
 import DailyForecast from "@/components/widgets/daily-forecast";
+import CurrentWeatherWidget from "@/components/widgets/current-data";
+import MapBox from "@/components/widgets/mapbox";
+import LargeCities from "@/components/widgets/large-cities";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col gap-10 bg-gray-900">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="w-5/6 md:w-3/4 mx-auto flex-1 flex flex-col">
-        <div id="current-data" className="flex flex-col md:flex-row md:justify-between md:items-start gap-4">
-          <div className="min-w-64 space-y-4">
+      <div className="w-full sm:w-3/4 px-2 mx-auto flex-1">
+        <div className="grid lg:grid-rows-3 lg:grid-cols-3 xl:grid-rows-4 xl:grid-cols-4 gap-2">
+          <div className="lg:row-span-3 lg:col-span-1 xl:row-span-4 flex flex-col gap-2">
             <MainWeatherWidget />
             <DailyForecast />
           </div>
           <CurrentWeatherWidget />
+          <div className="lg:row-span-1 lg:col-span-2 xl:row-span-2 xl:col-span-3 flex flex-col lg:flex-row lg:justify-between gap-2">
+            <MapBox />
+            <LargeCities />
+          </div>
         </div>
       </div>
       <Footer />
---
+++ src/components/widgets/large-cities.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useWeatherContext } from "@/contexts/weather";
+
+export default function LargeCities() {
+    const { fetchWeatherData } = useWeatherContext();
+    const largeCities = [
+        {
+            cityName: "New York",
+            latitude: 40.7127281,
+            longitude: -74.0060152,
+        },
+        {
+            cityName: "Los Angeles",
+            latitude: 34.0536909,
+            longitude: -118.242766,
+        },
+        {
+            cityName: "Toronto",
+            latitude: 43.6534817,
+            longitude: -79.3839347,
+        },
+        {
+            cityName: "London",
+            latitude: 51.5073219,
+            longitude: -0.1276474,
+        },
+        {
+            cityName: "Tokyo",
+            latitude: 35.6828387,
+            longitude: 139.7594549,
+        },
+        {
+            cityName: "Beijing",
+            latitude: 39.906217,
+            longitude: 116.3912757,
+        },
+        {
+            cityName: "Sidney",
+            latitude: -33.8698439,
+            longitude: 151.2082848,
+        }
+    ];
+
+    const submitHandler = (largeCity: { cityName: string, latitude: number, longitude: number}) => {
+        const { cityName, latitude, longitude } = largeCity;
+        fetchWeatherData(cityName, latitude, longitude);
+    };
+    
+    return (
+        <div id="large-cities" className="w-full lg:w-1/4 p-6 flex flex-col gap-2 text-white bg-neutral-900 border border-neutral-800 rounded-2xl">
+            <h2 className="text-sm">OTHER LARGE CITIES</h2>
+            <menu className="flex-1 flex flex-col justify-between">
+                {
+                    largeCities.map((largeCity) => {
+                        return (
+                            <button 
+                                className="block w-full text-left p-2.5 hover:bg-white hover:bg-opacity-10 rounded-lg border border-neutral-800"
+                                onClick={() => submitHandler(largeCity)}
+                            >
+                                {largeCity.cityName}
+                            </button>
+                        )
+                    })
+                }
+            </menu>
+        </div>
+    );
+}
